f7b65cf1991ee4f29b2b4ce26759ca5d30634a23
aariamidu <aarifah.amidu@gmail.com>
2022-11-07 16:12:12 +0000

    current temperature
    added temp

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = Math.round(response.data.name);
+  descriptionElement.innerHTML = Math.round(
+    response.data.weather[0].description
+  );
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
 }
 let apiKey = "3a347c4310cafc46o98tad7838da2e8b";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=London&key={apikey}&units=metric";
+  "https://api.shecodes.io/weather/v1/current?query=London&key={apiKey}&units=metric";
 axios.get(apiUrl).then(displayTemperature);
