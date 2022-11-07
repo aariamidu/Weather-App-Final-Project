2886cbdb35c8addabf422b1c686faccc264f59ea
aariamidu <aarifah.amidu@gmail.com>
2022-11-07 15:49:09 +0000

    js
    api being linked

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.main.temp;
+}
+let apiKey = "3a347c4310cafc46o98tad7838da2e8b";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=London&key={apikey}&units=metric";
+axios.get(apiUrl).then(displayTemperature);
